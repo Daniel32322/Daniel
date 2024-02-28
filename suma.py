16946bb9b045111d74e36090622c54765472e73e
Daniel32322 <159713197+Daniel32322@users.noreply.github.com>
2024-02-28 19:29:22 -0300

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,11 @@
+def main():
+    
+    num1 = input("Enter a number:")
+    num1 = int(num1)
+    num2 = input("Enter another:")
+    num2 = int(num2)
+
+    print("%d + %d = %d" % (num1, num2, (num1+num2)))
+
+# call the main function:
+main()
